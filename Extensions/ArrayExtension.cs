d281a5c74d271577423020215e21c935d472bae7
Gaein_nidb <mail@gaein.cn>
2021-07-25 22:54:15 +0800

    Add extension method

---
+++ Extensions/ArrayExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HaoEncoding.Extensions
+{
+    public static class ArrayExtension
+    {
+        public static int CopyToCount<T>(this IList<T> source, IList<T> array, int index = 0)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (source.Count + index > array.Count)
+                throw new ArgumentOutOfRangeException(nameof(source));
+
+            var count = 0;
+            while (count < source.Count)
+            {
+                
+                array[index + count] = source[count];
+                ++count;
+            }
+
+            return count;
+        }
+
+        public static int CopyToCount<T>(this IList<T> source, Span<T> destination, int index = 0)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (source.Count + index > destination.Length)
+                throw new ArgumentOutOfRangeException(nameof(source));
+
+            var count = 0;
+            while (count < source.Count)
+            {
+
+                destination[index + count] = source[count];
+                ++count;
+            }
+
+            return count;
+        }
+    }
+}
